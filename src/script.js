d1fb5fd0eb2551aa61cd427db2d93361cc175a77
Kirstface <91326105+Kirstface@users.noreply.github.com>
2021-10-31 14:41:58 -0400

    Added Weekly Forecast
    Moved HTML to JS Function for weekly forecast and deleted the fake weather data and added an OpenWeather API for proper weather in current location. Also, applied filters to the icons to suit the color-way better.

--- src/script.js
+++ src/script.js
@@ -35,10 +35,8 @@ function searchCity(city) {
 }
 
 function displayWeather(response) {
-  //console.log(response.data);
-
+  console.log(response.data);
   let iconElement = document.querySelector("#icon-weather");
-
   document.querySelector("#city-title").innerHTML = response.data.name;
   findCelsiusTemperature = response.data.main.temp;
   document.querySelector("#main-number-temp").innerHTML = Math.round(
@@ -56,6 +54,54 @@ function displayWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let dailyForecast = document.querySelector("#daily-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 7) {
+      forecastHTML =
+        forecastHTML +
+        `                <div class="col-sm">
+                    <h6 class="weatherText dayOfWeek">${formatDay(
+                      forecastDay.dt
+                    )}</h6>
+                    <img src="http://openweathermap.org/img/wn/${
+                      forecastDay.weather[0].icon
+                    }@2x.png" alt="" width="40" class="forecastIcon">
+
+<h6 class="weatherText">
+<div class="forecastDescription"><span class="weekTempHighs">${Math.round(
+          forecastDay.temp.max
+        )}° |</span> 
+<span class="weekTempLows">${Math.round(forecastDay.temp.min)}° </span>
+</div></h6>
+
+
+                </div>
+                    `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  dailyForecast.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "6ab671fa732a8fecfd76817bf10a3006";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function currentPosition(position) {
